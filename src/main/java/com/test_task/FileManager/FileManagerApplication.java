a6f7cf2c032e670df3af453f65aee84a1a5ebf0b
Leonid <x.1.x2@yandex.ru>
2024-07-18 14:22:32 +0400

    Initial commit

---
+++ src/main/java/com/test_task/FileManager/FileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.test_task.FileManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerApplication.class, args);
+	}
+
+}
